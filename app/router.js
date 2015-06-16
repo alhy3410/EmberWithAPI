a55e37856f34609b1164ca913e8679cf8348fb92
andrew lee & ian bevel <student@epicodus.com>
2015-06-16 12:15:10 -0700

    legislators look up by zipcode

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('home', {path: '/'}),
+  this.resource('legislators', function() {
+    this.resource('contact', {path: 'contact/:zip'});
+  });
 });
 
 export default Router;
